6d2127cdb73a2623d8501500dc00482a2055779a
Марина Чазова < >
2017-08-31 00:04:12 +0300

    Add fio validation

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+ var div = document.getElementById('submitButton');
 
-//.getElementsByTagName('body')[0].innerHTML = "hop";
-//alert("hey");
+ div.addEventListener('click', validate, false);
+
+ function validate(e) {
+ 	var regexp = /^[A-Za-zА-Яа-я]+\s[A-Za-zА-Яа-я]+\s[A-Za-zА-Яа-я]+$/;
+ 	var fio = document.getElementsByName("fio")[0].value;
+	console.log(e.target);
+	console.log(regexp.test(fio));
+ 	
+ 	return regexp.test(fio);
+ }
